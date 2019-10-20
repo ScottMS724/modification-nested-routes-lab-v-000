19160362dee7de981117d048ede7da4c66ed8464
Scott Silverman <scottms724@gmail.com>
2019-10-20 15:39:34 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else 
+      @song = Song.new(artist_id: params[:artist_id])
+    end 
   end
 
   def create
@@ -39,7 +43,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
   end
 
   def update
